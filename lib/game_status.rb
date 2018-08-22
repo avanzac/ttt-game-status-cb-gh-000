2430b1977601cf851402c370a389f453e07572aa
darren mcgregor <darren.mcgregor@avanzac.co.uk>
2018-08-22 11:43:37 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -6,6 +6,8 @@ def display_board(board)
     puts " #{board[6]} | #{board[7]} | #{board[8]} "
   end
 
+  board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
 # Helper Method
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
