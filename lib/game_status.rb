fb939cac67a7a3cfebd553e3b5f4306a7135b2f6
darren mcgregor <darren.mcgregor@avanzac.co.uk>
2018-08-22 12:43:42 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,5 +1,4 @@
 
-
 # Helper Method
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
@@ -7,33 +6,33 @@ end
 
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [
-  [0,1,2], # top row
-  [3,4,5], # middle row
-  [6,7,8], # bottom row
-  [0,3,6], # left column
-  [1,4,7], # middle column
-  [2,5,8], # right column
-  [0,4,8], # diagonal a
-  [6,4,2] # diagonal b
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [6, 4, 2]
 ]
 
 def won?(board)
-  WIN_COMBINATIONS.each { | win_combination |
-    win_index_0 = win_combination[0]
-    win_index_1 = win_combination[1]
-    win_index_2 = win_combination[2]
+  WIN_COMBINATIONS.each {|win_combo|
+    index_0 = win_combo[0]
+    index_1 = win_combo[1]
+    index_2 = win_combo[2]
 
-    position_1 = board(win_index_0)
-    position_2 = board(win_index_1)
-    position_3 = board(win_index_2)
+    position_1 = board[index_0]
+    position_2 = board[index_1]
+    position_3 = board[index_2]
 
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
-     return win_combination
-   elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
-     return win_combination
-  end
-}
-return false
+      return win_combo
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+      return win_combo
+    end
+  }
+  return false
 end
 
 def full?(board)
@@ -63,9 +62,9 @@ def winner(board)
     return nil
   else
     if board[index[0]] == "X"
-     return "X"
+      return "X"
     else
-     return "O"
+      return "O"
+    end
   end
-end
 end
