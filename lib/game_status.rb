a62a63bc4c96ee0221917559cfdcdf5484e542d8
darren mcgregor <darren.mcgregor@avanzac.co.uk>
2018-08-22 12:53:42 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -7,18 +7,18 @@ end
 
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [
-  [0, 1, 2], # top row
-  [3, 4, 5], # middle row
-  [6, 7, 8], # bottom row
-  [0, 3, 6], # left column
-  [1, 4, 7], # middle column
-  [2, 5, 8], # right column
-  [0, 4, 8], # diagonal a
-  [6, 4, 2] # diagonal b
+  [0,1,2], # top row
+  [3,4,5], # middle row
+  [6,7,8], # bottom row
+  [0,3,6], # left column
+  [1,4,7], # middle column
+  [2,5,8], # right column
+  [0,4,8], # diagonal a
+  [2,4,6] # diagonal b
 ]
 
 def won?(board)
-  WIN_COMBINATIONS.each {|win_combination|
+  WIN_COMBINATIONS.each do | win_combination |
     win_index_0 = win_combination[0]
     win_index_1 = win_combination[1]
     win_index_2 = win_combination[2]
@@ -27,17 +27,13 @@ def won?(board)
     position_2 = board(win_index_1)
     position_3 = board(win_index_2)
 
-    if position_1 == "X" && position_2 == "X" && position_3 == "X"
-     return win_combination
-   elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
-     return win_combination
-  end
-}
-return false
+    position_1 == position_2 && position_2 == position_3 && position_taken?(board, win_index_1) #detect returns first element (position_1) & make sure position is taken (that it's either an X or O).
+
+end
 end
 
 def full?(board)
-  board.all? {|index| index == "X" || index == "O"}
+  board.all? {|i| i == "X" || i == "O"}
 end
 
 def draw?(board)
@@ -57,15 +53,7 @@ def over?(board)
 end
 
 def winner(board)
-  index = []
-  index = won?(board)
-  if index == false
-    return nil
-  else
-    if board[index[0]] == "X"
-     return "X"
-    else
-     return "O"
-  end
+  if won?(board)
+    return board[won?(board) [0]]
 end
 end
