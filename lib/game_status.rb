6cde56aea0e6c8af23300f3905d921e1095ccb64
darren mcgregor <darren.mcgregor@avanzac.co.uk>
2018-08-22 12:31:48 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -18,7 +18,7 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
-  WIN_COMBINATIONS.each do | win_combination |
+  WIN_COMBINATIONS.each { | win_combination |
     win_index_1 = win_combination[0]
     win_index_2 = win_combination[1]
     win_index_3 = win_combination[2]
@@ -27,9 +27,13 @@ def won?(board)
     position_2 = board(win_index_2)
     position_3 = board(win_index_3)
 
-    position_1 == position_2 && position_2 == position_3 && position_taken?(board,win_index_1)
-
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+     return win_combination
+   elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+     return win_combination
   end
+}
+return false
 end
 
 def full?(board)
